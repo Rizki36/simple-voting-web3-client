909e24ca743dccaa32232dbfc340952041a5bccc
Fitra Rahman <39044004+Rizki36@users.noreply.github.com>
2025-05-24 01:08:52 +0700

    chore: add dependencies & components

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "@/components/ui/sonner";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRoute({
 	component: () => (
 		<>
+			<Toaster />
 			<Outlet />
 			<TanStackRouterDevtools />
 		</>
