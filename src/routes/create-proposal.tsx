e7c79f7391f4501a46fff777709aa26fa6793407
Fitra Rahman <39044004+Rizki36@users.noreply.github.com>
2025-05-24 16:06:23 +0700

    refactor: Standardize formatting and improve readability across multiple components

--- src/routes/create-proposal.tsx
+++ src/routes/create-proposal.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router';
-import CreateProposalPage from '../components/pages/CreateProposal/CreateProposalPage';
+import { createFileRoute } from "@tanstack/react-router";
+import CreateProposalPage from "../components/pages/CreateProposal/CreateProposalPage";
 
-export const Route = createFileRoute('/create-proposal')({
-  component: CreateProposalPage,
+export const Route = createFileRoute("/create-proposal")({
+	component: CreateProposalPage,
 });
